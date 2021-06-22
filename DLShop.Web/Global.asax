aa0a08ffc728b0caa8e66ed242a35f1a760a4b57
PhanDuc11 <phanminhduc012@gmail.com>
2021-06-22 13:40:30 +0700

    the first commit

---
+++ DLShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DLShop.Web.MvcApplication" Language="C#" %>
---
+++ DLShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DLShop.Web.Startup))]
+namespace DLShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
